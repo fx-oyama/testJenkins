f92cb916176f5beeb7b5852d2af04e6d3332f6c0
Oyama Yoichi <oyama.yoichi@fujixerox.co.jp>
2015-08-07 14:27:50 +0900

    testddddddd

--- TestPro001/src/com/fx/ProMain.java
+++ TestPro001/src/com/fx/ProMain.java
@@ -21,21 +21,7 @@ public final class ProMain {
      */
     public static void main(final String[] args) {
 
-        test001();
+        System.out.println("---test---");
 
     }
-
-    /**
-     * test.
-     */
-    private static void test001() {
-        CalcUtil ut = new CalcUtil();
-        int val = 0;
-
-        val = ut.calculateValue(1, 2);
-
-        System.out.println("val = " + val);
-
-    }
-
 }
